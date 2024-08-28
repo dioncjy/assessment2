9912044a1dda622f41357b013614fb9738fd5046
dioncheng <dioncheng98@gmail.com>
2024-08-28 23:56:23 +0800

    fix: changed file structure for lmd py file

--- assessment2-terraform-aws/main.tf
+++ assessment2-terraform-aws/main.tf
@@ -5,10 +5,10 @@ provider "aws" {
 module "lambda_function" {
   source                    = "./modules/lambda_function"  
   iam_role_name             = "lambda_exec_role"
-  lambda_function_filename  = "./lambda_function/function.zip"
+  lambda_function_filename  = "./modules/lambda_function/function.zip"
   lambda_function_name      = "govtech-devops-q2"
   lambda_handler            = "lambda_function.lambda_handler"
   lambda_runtime            = "python3.8"
-  source_file               = "./lambda_function/lambda_function.py"
-  output_path               = "./lambda_function/function.zip"
+  source_file               = "./modules/lambda_function/lambda_function.py"
+  output_path               = "./modules/lambda_function/function.zip"
 }
